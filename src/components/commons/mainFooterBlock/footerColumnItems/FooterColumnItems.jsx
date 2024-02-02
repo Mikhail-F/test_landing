5fd609226aa7e0ad32cb0a24c6f9f3172aaaced7
MishaT <tabunov-mishia@mail.ru>
2024-02-02 21:04:19 +0500

    test

--- src/components/commons/mainFooterBlock/footerColumnItems/FooterColumnItems.jsx
+++ src/components/commons/mainFooterBlock/footerColumnItems/FooterColumnItems.jsx
@@ -2,13 +2,13 @@ import styles from "./FooterColumnItems.module.sass";
 
 export function FooterColumnItems({ title, items }) {
   return (
-    <div className={styles.contacts__block}>
+    <div key={title} className={styles.contacts__block}>
       <h3 className={styles.title}>{title}</h3>
       {items.map((el) => contactItem(el))}
     </div>
   );
 
   function contactItem(text) {
-    return <a href={"/"} className={styles.item}>{text}</a>;
+    return <a key={text} href={"/"} className={styles.item}>{text}</a>;
   }
 }
--- src/components/pages/contactUsPage/contactUs/contactUsBlockLeft/ContactUsBlockLeft.jsx
+++ src/components/pages/contactUsPage/contactUs/contactUsBlockLeft/ContactUsBlockLeft.jsx
@@ -2,7 +2,7 @@ import styles from "./ContactUsBlockLeft.module.sass";
 
 export function ContactUsBlockLeft({ title, items }) {
   return (
-    <div className={styles.contact__block}>
+    <div key={title} className={styles.contact__block}>
       <p className={styles.title}>{title}</p>
       {items.map((el) => (
         <ContactUsContent item={el} />
@@ -13,7 +13,7 @@ export function ContactUsBlockLeft({ title, items }) {
 
 function ContactUsContent({ item }) {
   return item.img ? (
-    <div className={styles.contact__data}>
+    <div key={item.subtitle} className={styles.contact__data}>
       <img
         src={item.img}
         className={styles.contact__img}
@@ -22,6 +22,8 @@ function ContactUsContent({ item }) {
       <p className={styles.subtitle}>{item.subtitle}</p>
     </div>
   ) : (
-    <p className={styles.subtitle}>{item.subtitle}</p>
+    <p key={item.subtitle} className={styles.subtitle}>
+      {item.subtitle}
+    </p>
   );
 }
